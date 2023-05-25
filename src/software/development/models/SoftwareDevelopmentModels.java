6bf026647126a43e90a05287b4931352be24fe04
user <user@Laptop>
2023-05-25 14:50:10 -0400

    Message test. First commit.

---
+++ src/software/development/models/SoftwareDevelopmentModels.java
@@ -0,0 +1,34 @@
+package software.development.models;
+
+
+public class SoftwareDevelopmentModels {
+
+    
+    public static void main(String[] args) {
+        /*
+        Software Development Life Cycle:
+        Planning - Taking input from people and using them to identify problems and create solutions.
+        Design - Software developers create a design plan. Stakeholders review it and offer feedback. The design outlines how the software functions and technologies used.
+        Testing - Software is tested for bugs and meeting requirements.
+        Deployment - Software is launched to the production environment and able to be used by end-users.
+        Maintenance - While testing eliminates lots of errors, there are usually problems that slip by. Users can submit bug reports.
+        
+        Since it is a cycle, the steps will keep on repeating after the software is launched.
+        */
+        
+        /*
+        Different Software Development Models
+        Waterfall: Defining requirements, design, implementation and coding, testing, deployment, maintenance.
+        
+        Agile: The definition of agile is to move quickly and easily. Agile is a set of values and principles that provide a guideline on how to create high quality software
+                in as little time as possible.
+            Principles:
+            - Pinpointing the most important features, launching, then adding other things later.
+            
+            Methodoloy:
+            - Scrum
+            - Kanban
+        */
+    }
+    
+}
